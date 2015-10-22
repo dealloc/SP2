df4c394e54b89015e1929a84ec69d43e1473d866
Jérémy Thiebaut <jeremy.thb@gmail.com>
2015-10-22 11:19:59 +0200

    testing commit branch jeremy

--- core/src/be/ehb/spg3/Dummy.java
+++ core/src/be/ehb/spg3/Dummy.java
@@ -1,6 +1,7 @@
 package be.ehb.spg3;
-// Created by Wannes Gennar. All rights reserved
 
+// Created by Jérémy Thiebaut. All rights reserved
+//test branch jeremy
 public class Dummy
 {
 	public static String dummy()
