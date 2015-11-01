59430474bffb232bc569effa66eafe16abe0d4b5
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-01 21:05:48 +0100

    fixed removed function

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -2,7 +2,6 @@ package be.ehb.spg3;
 // Created by Wannes Gennar. All rights reserved
 
 
-import be.ehb.spg3.realms.database.CoreRealm;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -16,7 +15,6 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage)
 	{
-		CoreRealm.test();
 		stage.setTitle("software project - 3");
 		stage.show();
 	}
