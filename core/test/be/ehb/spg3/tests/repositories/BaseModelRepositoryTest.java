8b81e4f78eb876503d7bf5ebfc66ac17d9610f25
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-03 08:00:34 +0100

    added test class for basemodelrepository

---
+++ core/test/be/ehb/spg3/tests/repositories/BaseModelRepositoryTest.java
@@ -0,0 +1,56 @@
+package be.ehb.spg3.tests.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// Created by Wannes Gennar. All rights reserved
+public class BaseModelRepositoryTest
+{
+	@Before
+	public void setUp() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testSave() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testFind() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testFindOrFail() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testFindByField() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testFindByField1() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testFindByField2() throws Exception
+	{
+
+	}
+
+	@Test
+	public void testGetAll() throws Exception
+	{
+
+	}
+}
