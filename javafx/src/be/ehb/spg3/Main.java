33da2d8d30e435b13762d6c5fd73fb2e8727719a
Jeremy Thiebaut <jeremy.thb@gmail.com>
2015-11-19 10:50:20 +0100

    Login GUI runnen in main + aanpassing voor login as guest

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -3,19 +3,25 @@ package be.ehb.spg3;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application
 {
-	public static void main(String[] args)
-	{
-		launch(args);
-	}
-	
 	@Override
-	public void start(Stage stage)
-	{
-		stage.setTitle("software project - 3");
-		stage.show();
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("design/login/login.fxml"));
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setResizable(false);
+		primaryStage.centerOnScreen();
+		primaryStage.setScene(new Scene(root, 618, 480));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
