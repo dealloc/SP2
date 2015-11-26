d01bb592aeb8f2f546712e943000639f38207651
Wannes Gennar <wannes.gennar@gmail.com>
2015-11-26 10:20:39 +0100

    setup foreign collections

--- core/src/be/ehb/spg3/entities/questions/Question.java
+++ core/src/be/ehb/spg3/entities/questions/Question.java
@@ -3,8 +3,6 @@ package be.ehb.spg3.entities.questions;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-import java.util.List;
-
 // Created by Wannes Gennar. All rights reserved
 
 /**
@@ -14,6 +12,5 @@ import java.util.List;
 public class Question
 {
 	@DatabaseField(id = true) private int id;
-	@DatabaseField private List<Question> questions; // TODO a list of questions in a list; whut?
 	@DatabaseField private String question;
 }
