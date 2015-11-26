6635b4de5e0d7cde21c6544a0067d7d2ff5c36e8
Jeremy Thiebaut <jeremy.thb@gmail.com>
2015-11-26 10:57:29 +0100

    pushen van alle gemaakte designs

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -7,17 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application
 {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("design/login/login.fxml"));
-		primaryStage.initStyle(StageStyle.UNDECORATED);
-		primaryStage.setResizable(false);
-		primaryStage.centerOnScreen();
-		primaryStage.setScene(new Scene(root, 618, 480));
+		Parent root = FXMLLoader.load(getClass().getResource("design/moderator/dashboard.fxml"));
+		primaryStage.setScene(new Scene(root, 1280, 720));
+		//primaryStage.initStyle(StageStyle.UNDECORATED);
+		//primaryStage.setResizable(false);
+		//primaryStage.centerOnScreen();
+		primaryStage.setMinWidth(800);
+		primaryStage.setMinHeight(600);
 		primaryStage.show();
 	}
 
