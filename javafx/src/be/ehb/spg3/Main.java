5df0234f0b84ce1ff30c74b1d78f70b90dec2c4d
Wannes Gennar <wannes.gennar@gmail.com>
2015-12-02 20:43:37 +0100

    load admin panel

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -2,12 +2,13 @@ package be.ehb.spg3;
 // Created by Wannes Gennar. All rights reserved
 
 
+import com.guigarage.flatterfx.FlatterFX;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 
 public class Main extends Application
@@ -20,24 +21,24 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-		Parent root = FXMLLoader.load(getClass().getResource("auth/login/login.fxml"));
-		stage.initStyle(StageStyle.UNDECORATED);
-		stage.setResizable(false);
-		stage.centerOnScreen();
-		stage.setScene(new Scene(root, 618, 480));
+//		Parent root = FXMLLoader.load(getClass().getResource("auth/login/login.fxml"));
+//		stage.initStyle(StageStyle.UNDECORATED);
+//		stage.setResizable(false);
+//		stage.centerOnScreen();
+//		stage.setScene(new Scene(root, 618, 480));
+//		stage.show();
+
+		Parent root = FXMLLoader.load(getClass().getResource("design/Admin/AdminHome.fxml"));
+		Scene myScene = new Scene(root);
+		stage.setTitle("Software Project 2 - Groep 3");
+		stage.setScene(myScene);
+		stage.setFullScreen(true);
+		stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+		stage.setMinWidth(800);
+		stage.setMinHeight(600);
 		stage.show();
 
-//		Parent root = FXMLLoader.load(getClass().getResource("design/Admin/AdminHome.fxml"));
-//		Scene myScene = new Scene(root, 1280, 720);
-//		stage.setTitle("Software Project 2 - Groep 3");
-//		stage.setScene(myScene);
-//		//stage.setFullScreen(true);
-//		//stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-//		stage.setMinWidth(800);
-//		stage.setMinHeight(600);
-//		stage.show();
-//
-//		FlatterFX.style();
+		FlatterFX.style();
 
 	}
 }
