a500d566c7987b863211c3e7cd75c59323c064a4
Wannes Gennar <wannes.gennar@gmail.com>
2015-12-03 08:14:12 +0100

    implemented switching screens through event bus

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -5,12 +5,7 @@ package be.ehb.spg3;
 import be.ehb.spg3.contracts.events.EventBus;
 import be.ehb.spg3.events.SwitchScreenEvent;
 import be.ehb.spg3.events.handlers.SwitchScreenHandler;
-import com.guigarage.flatterfx.FlatterFX;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 import static be.ehb.spg3.providers.InjectionProvider.resolve;
@@ -26,28 +21,9 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-//		Parent root = FXMLLoader.load(getClass().getResource("auth/login/login.fxml"));
-//		stage.initStyle(StageStyle.UNDECORATED);
-//		stage.setResizable(false);
-//		stage.centerOnScreen();
-//		stage.setScene(new Scene(root, 618, 480));
-//		stage.show();
+		resolve(EventBus.class).subscribe(new SwitchScreenHandler(stage));
 
-		resolve(EventBus.class).subscribe(new SwitchScreenHandler());
-
-		resolve(EventBus.class).fire(new SwitchScreenEvent("design/Admin/AdminHome.fxml", false));
-
-		Parent root = FXMLLoader.load(getClass().getResource("design/Admin/AdminHome.fxml"));
-		Scene myScene = new Scene(root);
-		stage.setTitle("Software Project 2 - Groep 3");
-		stage.setScene(myScene);
-		stage.setFullScreen(true);
-		stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-		stage.setMinWidth(800);
-		stage.setMinHeight(600);
-		stage.show();
-
-		FlatterFX.style();
-
+		resolve(EventBus.class).fire(new SwitchScreenEvent("design/Admin/AdminHome.fxml", true));
+//		resolve(EventBus.class).fire(new SwitchScreenEvent("design/login/login.fxml", false));
 	}
 }
--- javafx/src/be/ehb/spg3/events/handlers/SwitchScreenHandler.java
+++ javafx/src/be/ehb/spg3/events/handlers/SwitchScreenHandler.java
@@ -1,14 +1,74 @@
 package be.ehb.spg3.events.handlers;
 
 import be.ehb.spg3.events.SwitchScreenEvent;
+import com.guigarage.flatterfx.FlatterFX;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import net.engio.mbassy.listener.Handler;
+
+import java.io.IOException;
 
 // Created by Wannes Gennar. All rights reserved
 public class SwitchScreenHandler
 {
+	private Stage stage;
+	private SwitchScreenEvent event;
+
+	public SwitchScreenHandler(Stage stage)
+	{
+		this.stage = stage;
+	}
+
 	@Handler
 	public void handle(SwitchScreenEvent event)
 	{
-		System.out.println(event.getLocation().toString());
+		this.event = event;
+		Platform.runLater(this::showWindow);
+	}
+
+	private void showWindow()
+	{
+		try
+		{
+			Parent root = FXMLLoader.load(event.getLocation()); // attempt to load first. if this fails nothing changes
+			if (this.stage.isShowing())
+			{
+				this.stage.close();
+				this.stage = new Stage();
+			}
+
+			Scene myScene = new Scene(root);
+			stage.setTitle("Software Project 2 - Groep 3");
+			if (event.getFullscreen()) this.setFullscreen();
+			else this.setWindowed();
+			stage.setScene(myScene);
+			stage.show();
+
+			FlatterFX.style();
+		}
+		catch (IOException io)
+		{
+			// TODO fire event
+		}
+	}
+
+	private void setFullscreen()
+	{
+		stage.setFullScreen(true);
+		stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+		stage.setMinWidth(800);
+		stage.setMinHeight(600);
+	}
+
+	private void setWindowed()
+	{
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.setResizable(false);
+		stage.centerOnScreen();
 	}
 }
