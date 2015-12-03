8daece11a252771d30326fdc52c00f51852ee38e
Wannes Gennar <wannes.gennar@gmail.com>
2015-12-03 16:42:25 +0100

    proof of concept pane switching

--- javafx/src/be/ehb/spg3/controllers/AdminController.java
+++ javafx/src/be/ehb/spg3/controllers/AdminController.java
@@ -4,18 +4,27 @@ package be.ehb.spg3.controllers;
  * Created by Jeroen_2 on 2/12/2015.
  */
 
-import javafx.application.Platform;
+import be.ehb.spg3.contracts.events.EventBus;
+import be.ehb.spg3.events.SwitchPaneEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import net.engio.mbassy.listener.Handler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static be.ehb.spg3.providers.InjectionProvider.resolve;
 
 
 public class AdminController implements Initializable
 {
 
+	@FXML
+	public AnchorPane contentRoot;
 	@FXML
 	private Label lblUserName;
 
@@ -23,10 +32,29 @@ public class AdminController implements Initializable
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources)
 	{
 		this.lblUserName.setText("Jeroen");
+		resolve(EventBus.class).subscribe(this); // register ourselves as an event listener
+		resolve(EventBus.class).fireSynchronous(new SwitchPaneEvent("design/admin/editProfile.fxml"));
 	}
 
 	public void close()
 	{
-		Platform.exit();
+		resolve(EventBus.class).fireSynchronous(new SwitchPaneEvent("design/admin/dashboard.fxml"));
+//		Platform.exit();
+	}
+
+	@Handler
+	public void changePanel(SwitchPaneEvent event)
+	{
+		Parent pane = null; // TODO: very dirty
+		try
+		{
+			pane = FXMLLoader.load(event.getLocation());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		this.contentRoot.getChildren().clear();
+		this.contentRoot.getChildren().add(pane);
 	}
 }
--- javafx/src/be/ehb/spg3/events/SwitchPaneEvent.java
+++ javafx/src/be/ehb/spg3/events/SwitchPaneEvent.java
@@ -1,6 +1,21 @@
 package be.ehb.spg3.events;
 
+import be.ehb.spg3.Main;
+
+import java.net.URL;
+
 // Created by Wannes Gennar. All rights reserved
 public class SwitchPaneEvent
 {
+	private URL location;
+
+	public SwitchPaneEvent(String location)
+	{
+		this.location = Main.class.getResource(location);
+	}
+
+	public URL getLocation()
+	{
+		return location;
+	}
 }
