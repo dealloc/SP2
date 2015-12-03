9c6aec68ecc7caaa32ccef37251cb79ff0379c28
Anthony <tisdenanthony@gmail.com>
2015-12-03 17:17:15 +0100

    een paar kleine updates toegebracht aan de fxml files van de userPanels, op zoek naar iets dat me laat switchen tussen de open- en gesloten vragenlijsten page

--- javafx/src/be/ehb/spg3/controllers/closedQuestionsController.java
+++ javafx/src/be/ehb/spg3/controllers/closedQuestionsController.java
@@ -1,16 +1,29 @@
 package be.ehb.spg3.controllers;
 
+import be.ehb.spg3.contracts.events.EventBus;
+import be.ehb.spg3.events.SwitchScreenEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+import javax.swing.text.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static be.ehb.spg3.providers.InjectionProvider.resolve;
 
 // Created by Anthony Min. All rights reserved
 public class closedQuestionsController implements Initializable
 {
 	@FXML
 	private Button btnStart;
+	@FXML
+	private TableColumn name;
+	@FXML
+	private TableColumn publisher;
+	@FXML
+	private TableColumn quizDone;
 
 
 	@Override
@@ -21,7 +34,7 @@ public class closedQuestionsController implements Initializable
 
 	public void openOpenQuestions()
 	{
-
+		resolve(EventBus.class).fire(new SwitchScreenEvent("design/user/profile_fxml/userPaneClosedQuestions.fxml", false));
 	}
 
 	public void startQuiz()
--- javafx/src/be/ehb/spg3/controllers/openQuestionsController.java
+++ javafx/src/be/ehb/spg3/controllers/openQuestionsController.java
@@ -1,31 +1,47 @@
 package be.ehb.spg3.controllers;
 
+import be.ehb.spg3.contracts.events.EventBus;
+import be.ehb.spg3.events.SwitchScreenEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+import javax.swing.text.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static be.ehb.spg3.providers.InjectionProvider.resolve;
 
 // Created by Anthony Min. All rights reserved
 public class openQuestionsController implements Initializable
 {
 	@FXML
 	private Button btnStart;
+	@FXML
+	private Button btnSluitVragen;
+	@FXML
+	private TableColumn name;
+	@FXML
+	private TableColumn publisher;
+	@FXML
+	private TableColumn amountQ;
+
 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
-
 	}
 
 	public void openClosedQuestions()
 	{
-
+		resolve(EventBus.class).fire(new SwitchScreenEvent("design/user/profile_fxml/userPaneClosedQuestions.fxml", false));
 	}
 
 	public void startQuiz()
 	{
 		btnStart.setText("dit is de open controller");
+		name.setText("lol");
 	}
 }
