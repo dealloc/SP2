7018538ee79a02451da262d55e219b1c96ebe7d6
Wannes Gennar <wannes.gennar@gmail.com>
2015-12-03 18:08:12 +0100

    fixed bug with garbage collector cleaning up listener

--- javafx/src/be/ehb/spg3/Main.java
+++ javafx/src/be/ehb/spg3/Main.java
@@ -21,7 +21,8 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-		resolve(EventBus.class).subscribe(new SwitchScreenHandler(stage));
+		SwitchScreenHandler handler = new SwitchScreenHandler(stage);
+		resolve(EventBus.class).subscribe(handler);
 
 		resolve(EventBus.class).fire(new SwitchScreenEvent("design/admin/AdminHome.fxml", true));
 //		resolve(EventBus.class).fire(new SwitchScreenEvent("design/login/login.fxml", false));
