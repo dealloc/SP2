d6f6723e07f7c0c56fb919e829cc929e22780fa2
Jeroen Van den Broeck <jeroen.van.den.broeck@student.ehb.be>
2015-12-03 11:59:20 +0100

    Style tweaking

--- javafx/src/be/ehb/spg3/controllers/AdminController.java
+++ javafx/src/be/ehb/spg3/controllers/AdminController.java
@@ -1,14 +1,14 @@
-package be.ehb.spg3.design.Admin;
+package be.ehb.spg3.controllers;
 
 /**
  * Created by Jeroen_2 on 2/12/2015.
  */
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,22 +16,18 @@ import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
 
-    @FXML //  fx:id="myButton"
-    private Button myButton; // Value injected by FXMLLoader
     @FXML
     private Label lblUserName;
     @FXML
-    private AnchorPane apRoot;
-
+    private Button btnTest;
 
     @Override // This method is called by the FXMLLoader when initialization is complete
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        assert myButton != null : "fx:id=\"myButton\" was not injected: check your FXML file 'AdminHome.fxml'.";
         lblUserName.setText("Jeroen");
+        btnTest.setText("test");
     }
 
-    public void test() {
-        myButton.setText("You clicked me!");
-        myButton.setPrefWidth(250);
+    public void close() {
+        Platform.exit();
     }
 }
