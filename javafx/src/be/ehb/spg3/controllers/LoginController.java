015b1d126e3b42a16defe75f6dbad2555120edcb
Jeremy Thiebaut <jeremy.thb@gmail.com>
2015-12-09 23:06:23 +0100

    Verdere aanpassingen aan controllers, GUI's, ...

--- javafx/src/be/ehb/spg3/controllers/LoginController.java
+++ javafx/src/be/ehb/spg3/controllers/LoginController.java
@@ -38,6 +38,23 @@ public class LoginController
 		resolve(EventBus.class).fireSynchronous(new SwitchScreenEvent("design/register/register.fxml", false));
 	}
 
+	public void loginAsGuest()
+	{
+		if (!resolve(Authenticator.class).login("Guest", "guest"))
+		{
+			Notifications.create().darkStyle().text("OOPS ! Wrong username or password...").showError();
+		} else
+		{
+			Notifications.create().darkStyle().text("Welcome ! You are now logged in.").showConfirm();
+			resolve(EventBus.class).fire(new SwitchScreenEvent("design/panel.fxml", true));
+		}
+	}
+
+	public void forgotPass()
+	{
+		resolve(EventBus.class).fireSynchronous(new SwitchScreenEvent("design/passRecovery/passRecovery.fxml", false));
+	}
+
 	public void close()
 	{
 		Platform.exit();
